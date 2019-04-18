8260c0901d4e2ac84005c3e4c82eb32546e51c96
Anuraag Baishya <baishya.anuraag1920@gmail.com>
2019-04-18 11:55:48 -0400

    Added timeout

--- testcerts/get-certs.py
+++ testcerts/get-certs.py
@@ -19,24 +19,29 @@ def main(domain_file):
             domains = f.readlines()
         for d in domains:
             d = d.replace("\n", "")
+            filename = d + ".pem"
+            filename_chain = d + "-chain.pem" 
             print("Retrieving certificate from {}...".format(d), end='')
-            cmd = "openssl s_client -showcerts -connect " + d +":443"
+            cmd = "timeout 10 openssl s_client -showcerts -connect " + d +":443"
             cmdarg = shlex.split(cmd)
             p = sub.Popen(cmdarg,stdin =sub.PIPE, stdout=sub.PIPE,stderr=sub.PIPE)
             output, errors = (p.communicate())
-            certs = (find_between(output.decode('utf-8'), "-----BEGIN CERTIFICATE-----", "-----END CERTIFICATE-----"))
-            #certs = re.search(r'-----BEGIN CERTIFICATE-----(.*)-----END CERTIFICATE-----',output.decode('utf-8')) TODO Optional way to do it
-            #print("Cert from {0}:\n {1}".format(d, certs))
-            if len(certs) == 0:
-                cert = "NOT FOUND"
-                cert_chain = ["NOT FOUND"]
-                print("...failed") 
+            if output.decode('utf-8') == "":
+                cert = "TIMED OUT"
+                cert_chain = ["TIMED OUT"]
+                print("...timed out") 
             else:
-                print("...done")
-                cert = certs[0]
-                cert_chain = certs[1:]
-            filename = d + ".pem"
-            filename_chain = d + "-chain.pem" 
+                certs = (find_between(output.decode('utf-8'), "-----BEGIN CERTIFICATE-----", "-----END CERTIFICATE-----"))
+                #certs = re.search(r'-----BEGIN CERTIFICATE-----(.*)-----END CERTIFICATE-----',output.decode('utf-8')) TODO Optional way to do it
+                #print("Cert from {0}:\n {1}".format(d, certs))
+                if len(certs) == 0:
+                    cert = "NOT FOUND"
+                    cert_chain = ["NOT FOUND"]
+                    print("...failed") 
+                else:
+                    print("...done")
+                    cert = certs[0]
+                    cert_chain = certs[1:]
             with open (os.path.join("cert", filename), "w+") as f:
                 f.write(cert)
                 f.close()
